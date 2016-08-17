2da276652b83cc39c619fcd9a2930c0f53aefbf4
Luis Guzman <luis.guzman@metalab.csun.edu>
2016-08-16 17:23:37 -0700

    Actually add the api to the uri in the landing page

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -44,64 +44,64 @@
 			<h2>Single Entity</h2>
 			<dl>
 				<dt>Retrieves information of a single entity</dt>
-				<dd><a href="{{ url('members/email/steven.fitzgerald@csun.edu') }}">{{ url('members/email/steven.fitzgerald@csun.edu') }}</a></dd>	
+				<dd><a href="{{ url('api/members/email/steven.fitzgerald@csun.edu') }}">{{ url('api/members/email/steven.fitzgerald@csun.edu') }}</a></dd>	
 				{{-- <dd><a href="{{ url('members/id/100010526') }}">{{ url('members/id/100010526') }}</a></dd>			 --}}
 			</dl>
 
 			<h2>Departments</h2>
 			<dl>
 				<dt>Retrieves the list of all departments</dt>
-				<dd><a href="{{ url('departments') }}">{{ url('departments') }}</a></dd>
+				<dd><a href="{{ url('api/departments') }}">{{ url('api/departments') }}</a></dd>
 				<dt>Retrieves the list of all aadministrative departments</dt>
-				<dd><a href="{{ url('departments/administrative') }}">{{ url('departments/administrative') }}</a></dd>
+				<dd><a href="{{ url('api/departments/administrative') }}">{{ url('api/departments/administrative') }}</a></dd>
 				<dt>Retrieves a specific department</dt>
-				<dd><a href="{{ url('departments/189') }}">{{ url('departments/189') }}</a></dd>
-				<dd><a href="{{ url('departments/10132') }}">{{ url('departments/10132') }}</a></dd>
+				<dd><a href="{{ url('api/departments/189') }}">{{ url('api/departments/189') }}</a></dd>
+				<dd><a href="{{ url('api/departments/10132') }}">{{ url('api/departments/10132') }}</a></dd>
 				<dt>Retrieves all members of a specific department</dt>
-				<dd><a href="{{ url('departments/189/members') }}">{{ url('departments/189/members') }}</a></dd>
-				<dd><a href="{{ url('departments/10132/members') }}"> {{ url('departmens/10132/members') }}</a></dd>
+				<dd><a href="{{ url('api/departments/189/members') }}">{{ url('api/departments/189/members') }}</a></dd>
+				<dd><a href="{{ url('api/departments/10132/members') }}"> {{ url('api/departmens/10132/members') }}</a></dd>
 			</dl>
 
 			<h2>Colleges</h2>
 			<dl>
 				<dt>Retrieves the list of all the colleges along with the departments associated with their college.</dt>
-				<dd><a href="{{ url('colleges') }}">{{ url('colleges') }}</a></dd>
+				<dd><a href="{{ url('api/colleges') }}">{{ url('api/colleges') }}</a></dd>
 				<dt>Retrieves the list of all the chairs of the colleges</dt>
-				<dd><a href="{{ url('colleges/chairs') }}">{{ url('colleges/chairs') }}</a></dd>
+				<dd><a href="{{ url('api/colleges/chairs') }}">{{ url('api/colleges/chairs') }}</a></dd>
 				<dt>Retrieves a specific college with all the departments affiliated with that college</dt>
-				<dd><a href="{{ url('colleges/52') }}">{{ url('colleges/52') }}</a></dd>
+				<dd><a href="{{ url('api/colleges/52') }}">{{ url('api/colleges/52') }}</a></dd>
 				<dt>Retrieves all chairs of specific college</dt>
-				<dd><a href="{{ url('colleges/52/chairs') }}">{{ url('colleges/52/chairs') }}</a></dd>
+				<dd><a href="{{ url('api/colleges/52/chairs') }}">{{ url('api/colleges/52/chairs') }}</a></dd>
 			</dl>
 			
 			<h2>Committees</h2>
 			<dl>
 				<dt>Retrieves the list of all the committees</dt>
-				<dd><a href="{{ url('committees') }}">{{ url('committees') }}</a></dd>
+				<dd><a href="{{ url('api/committees') }}">{{ url('api/committees') }}</a></dd>
 				<dt>Retrieves information of a specific committee</dt>
-				<dd><a href="{{ url('committees/atc') }}">{{ url('committees/atc') }}</a></dd>
+				<dd><a href="{{ url('api/committees/atc') }}">{{ url('api/committees/atc') }}</a></dd>
 				<dt>Retrieves all members iwn a committee</dt>
-				<dd><a href="{{ url('committees/aggab/members') }}">{{ url('committees/aggab/members') }}</a></dd>
+				<dd><a href="{{ url('api/committees/aggab/members') }}">{{ url('api/committees/aggab/members') }}</a></dd>
 			</dl>	
 
 			<h2>Centers</h2>
 			<dl>
 				<dt>Retrieves the list of all the centers</dt>
-				<dd><a href="{{ url('centers') }}">{{ url('centers') }}</a></dd>
+				<dd><a href="{{ url('api/centers') }}">{{ url('api/centers') }}</a></dd>
 				<dt>Retrieves a specific center</dt>
-				<dd><a href="{{ url('centers/viscom') }}">{{ url('centers/viscom') }}</a></dd>
+				<dd><a href="{{ url('api/centers/viscom') }}">{{ url('api/centers/viscom') }}</a></dd>
 				<dt>Retrieves the list of all members in a center</dt>
-				<dd><a href="{{ url('centers/viscom/members') }}">{{ url('centers/viscom/members') }}</a></dd>
+				<dd><a href="{{ url('api/centers/viscom/members') }}">{{ url('api/centers/viscom/members') }}</a></dd>
 			</dl>
 
 			<h2>Institutes</h2>
 			<dl>
 				<dt>Retrieves the list of all institutes</dt>
-				<dd><a href="{{ url('institutes') }}">{{ url('institutes') }}</a></dd>
+				<dd><a href="{{ url('api/institutes') }}">{{ url('api/institutes') }}</a></dd>
 				<dt>Retrieves a specific institute</dt>
-				<dd><a href="{{ url('institutes/ichwb') }}">{{ url('institutes/ichwb') }}</a></dd>
+				<dd><a href="{{ url('api/institutes/ichwb') }}">{{ url('api/institutes/ichwb') }}</a></dd>
 				<dt>Retrieves the list of members in an institute</dt>
-				<dd><a href="{{ url('/institutes/ichwb/members') }}">{{ url('/institutes/ichwb/members') }}</a></dd>
+				<dd><a href="{{ url('api/institutes/ichwb/members') }}">{{ url('api/institutes/ichwb/members') }}</a></dd>
 			</dl>
 	</div>
 </body>
