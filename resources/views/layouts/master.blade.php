d1cbc7882ddfdc28bd5bb2ecce92564023a7e116
Luis Guzman <luis.guzman@metalab.csun.edu>
2016-11-21 17:11:38 -0800

    Add the high res metalab footer image and code syntax highlighting

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,8 +7,14 @@
 	<link rel="icon" type="image/x-icon" href="favicon.ico" type="image/x-icon">
 	<!-- FONT LIBS -->
 	<script type="text/javascript" src="https://use.typekit.net/gfb2mjm.js"></script>
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic">
-	<link rel="stylesheet" href="{!! url('css/metaphor.css') !!}">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="{!! url('css/metaphor.css') !!}">
+	<link rel="stylesheet" type="text/css" href="{!! url('css/tomorrow.min.css') !!}">
+	<style type="text/css">
+		.metalab-footer .metalab-branding img {
+		  width: 110px; }
+	</style>
 </head>
 <body>
 	<div class="section section--sm">
@@ -29,10 +35,9 @@
                 </div>
             </div>
         </div>
-		@include('partials.csun-footer')
+		{{-- @include('partials.csun-footer') --}}
 		@include('partials.metalab-footer')
-		<script type="text/javascript">
-			try{Typekit.load({ async: true });}catch(e){}
-		</script>
+		<script type="text/javascript">try{Typekit.load({ async: true });}catch(e){}</script>
 		<script type="text/javascript" src="{!! url('js/metaphor.js') !!}"></script>
+		<script type="text/javascript" src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
 </body>
--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -2,13 +2,14 @@
 @section('content')
 	      	<h2 id="introduction" class="type--header type--thin">Introduction</h2>
 	      	The Directory web service provides contact information about CSUN entities. The web service provides a gateway to access the information via a REST-ful API. The information is retrieved by creating a specific URI and giving values to filter the data. The data is provided by CSUN Central IT. The information that is returned is a JSON object that contains contact information about a particular person, center, college, department, etc; the format of the JSON object is as follows:</p>
-	      	<pre><code>
+	      	<pre>
+		      	<code class="prettyprint lang-json">
 {
   "status": "200",
   "success": "true",
   "type": "department",
   "department": {
-    "department_id": "academic_departments:189",
+    "department_id": "academic_departments:189",catalog and SOLAR. The web service provides a gateway to access the information via a REST-ful API. The information is retrieved by creating a specific URI and giving values to filter the data. The information that is returned is a JSON object that contains a set of courses or classes; the format of the JSON object is as follows:
     "college_id": "academic_groups:52",
     "entity_type": "Academic Department",
     "name": "Computer Science",
@@ -34,8 +35,9 @@
     ]
   }
 }	      	
-			</code></pre>
-	      	<h2 id="getting-started" class="type--header type--thin">Getting Started</h2>
+				</code>
+			</pre>
+	      	<h2 id="how-to" class="type--header type--thin">How to Use</h2>
 			<ol>
 				<li><strong>GENERATE THE URI:</strong> Find the usage that fits your need. Browse through subcollections, instances and query types to help you craft your URI.</li>
 				<li><strong>PROVIDE THE DATA:</strong> Use the URI to query your data. See the Usage Example section.</li>
@@ -45,50 +47,160 @@
 			<h3 class="type--thin">Examples</h3>
 			<strong>Retrieves information of a single entity</strong>
 			<ul class="list">
-				<li class="list__item"><a href="{{ url('api/members/steven.fitzgerald@csun.edu') }}">{{ url('api/members/steven.fitzgerald@csun.edu') }}</a></li>
+				<li class="list__item"><a href="{{ url('api/members/steven.fitzgerald@csun.edu') }}">{!! url('api/members/steven.fitzgerald@csun.edu') !!}</a></li>
 			</ul>
 			<strong>Retrieves information of departments</strong>
 			<ul class="list">
-				<li class="list__item"><a href="{{ url('api/departments') }}">{{ url('api/departments') }}</a></li>
-				<li class="list__item"><a href="{{ url('api/departments/administrative') }}">{{ url('api/departments/administrative') }}</a></li>
-				<li class="list__item"><a href="{{ url('api/departments/189') }}">{{ url('api/departments/189') }}</a></li>
-				<li class="list__item"><a href="{{ url('api/departments/10132') }}">{{ url('api/departments/10132') }}</a></li>
-				<li class="list__item"><a href="{{ url('api/departments/189/members') }}">{{ url('api/departments/189/members') }}</a></li>
-				<li class="list__item"><a href="{{ url('api/departments/10132/members') }}"> {{ url('api/departmens/10132/members') }}</a></li>
+				<li class="list__item"><a href="{{ url('api/departments') }}">{!! url('api/departments') !!}</a></li>
+				<li class="list__item"><a href="{{ url('api/departments/administrative') }}">{!! url('api/departments/administrative') !!}</a></li>
+				<li class="list__item"><a href="{{ url('api/departments/189') }}">{!! url('api/departments/189') !!}</a></li>
+				<li class="list__item"><a href="{{ url('api/departments/10132') }}">{!! url('api/departments/10132') !!}</a></li>
+				<li class="list__item"><a href="{{ url('api/departments/189/members') }}">{!! url('api/departments/189/members') !!}</a></li>
+				<li class="list__item"><a href="{{ url('api/departments/10132/members') }}"> {!! url('api/departmens/10132/members') !!}</a></li>
 			</ul>
 			<strong>Retrieves information of Colleges</strong>
 			<ul class="list">
-				<li class="list__item"><a href="{{ url('api/colleges') }}">{{ url('api/colleges') }}</a></li>
-				<li class="list__item"><a href="{{ url('api/colleges/chairs') }}">{{ url('api/colleges/chairs') }}</a></li>
-				<li class="list__item"><a href="{{ url('api/colleges/52') }}">{{ url('api/colleges/52') }}</a></li>
-				<li class="list__item"><a href="{{ url('api/colleges/52/chairs') }}">{{ url('api/colleges/52/chairs') }}</a></li>
+				<li class="list__item"><a href="{{ url('api/colleges') }}">{!! url('api/colleges') !!}</a></li>
+				<li class="list__item"><a href="{{ url('api/colleges/chairs') }}">{!! url('api/colleges/chairs') !!}</a></li>
+				<li class="list__item"><a href="{{ url('api/colleges/52') }}">{!! url('api/colleges/52') !!}</a></li>
+				<li class="list__item"><a href="{{ url('api/colleges/52/chairs') }}">{!! url('api/colleges/52/chairs') !!}</a></li>
 			</ul>
 			<strong>Retrieves information about Committees</strong>
 			<ul class="list">
-				<li class="list__item"><a href="{{ url('api/committees') }}">{{ url('api/committees') }}</a></li>
-				<li class="list__item"><a href="{{ url('api/committees/atc') }}">{{ url('api/committees/atc') }}</a></li>
-				<li class="list__item"><a href="{{ url('api/committees/aggab/members') }}">{{ url('api/committees/aggab/members') }}</a></li>
+				<li class="list__item"><a href="{{ url('api/committees') }}">{!! url('api/committees') !!}</a></li>
+				<li class="list__item"><a href="{{ url('api/committees/atc') }}">{!! url('api/committees/atc') !!}</a></li>
+				<li class="list__item"><a href="{{ url('api/committees/aggab/members') }}">{!! url('api/committees/aggab/members') !!}</a></li>
 			</ul>
 			<strong>Retrieves information about Centers</strong>
 			<ul class="list">
-				<li class="list__item"><a href="{{ url('api/centers') }}">{{ url('api/centers') }}</a></li>
-				<li class="list__item"><a href="{{ url('api/centers/viscom') }}">{{ url('api/centers/viscom') }}</a></li>
-				<li class="list__item"><a href="{{ url('api/centers/viscom/members') }}">{{ url('api/centers/viscom/members') }}</a></li>
+				<li class="list__item"><a href="{{ url('api/centers') }}">{!! url('api/centers') !!}</a></li>
+				<li class="list__item"><a href="{{ url('api/centers/viscom') }}">{!! url('api/centers/viscom') !!}</a></li>
+				<li class="list__item"><a href="{{ url('api/centers/viscom/members') }}">{!! url('api/centers/viscom/members') !!}</a></li>
 			</ul>
 			<strong>Retrieves information about Institutes</strong>
 			<ul class="list">
-				<li class="list__item"><a href="{{ url('api/institutes') }}">{{ url('api/institutes') }}</a></li>
-				<li class="list__item"><a href="{{ url('api/institutes/ichwb') }}">{{ url('api/institutes/ichwb') }}</a></li>
-				<li class="list__item"><a href="{{ url('api/institutes/ichwb/members') }}">{{ url('api/institutes/ichwb/members') }}</a></li>
+				<li class="list__item"><a href="{{ url('api/institutes') }}">{!! url('api/institutes') !!}</a></li>
+				<li class="list__item"><a href="{{ url('api/institutes/ichwb') }}">{!! url('api/institutes/ichwb') !!}</a></li>
+				<li class="list__item"><a href="{{ url('api/institutes/ichwb/members') }}">{!! url('api/institutes/ichwb/members') !!}</a></li>
 			</ul>
 	      	<h2 id="subcollections" class="type--header type--thin">Subcollections</h2>
 			<h3 class="type--thin">Examples</h3>
 			<strong>Retrieves information of a single entity</strong><br />
 			<ul class="list">
 				<li class="list__item">
-					<a href="{{ url('api/members?email=steven.fitzgerald@csun.edu') }}">{{ url('api/members?email=steven.fitzgerald@csun.edu') }}</a>
+					<a href="{{ url('api/members?email=steven.fitzgerald@csun.edu') }}">{!! url('api/members?email=steven.fitzgerald@csun.edu') !!}</a>
 				</li>
 			</ul>
-	      	<h2 id="instances" class="type--header type--thin">Instances</h2>
-	      	<h2 id="query" class="type--header type--thin">Query</h2>
+	      	<h2 id="usage-example" class="type--header type--thin">Usage Example</h2>
+	      		<dl class="accordion">
+		<dt class="accordion__header"> JavaScript <i class="fa fa-chevron-down fa-pull-right type--red" aria-hidden="true"></i></dt>
+		<dd class="accordion__content">
+	    	<pre>
+	    	<code class="prettyprint lang-js">
+// this example assumes jQuery integration for ease of use
+// and a &lt;div&gt; element with the ID of "course-results"
+
+// query all CompSci courses
+var url = '{!! url('api/departments/189') !!}';
+$(document).ready(function() {
+
+	// perform a shorthand AJAX call to grab the information
+	$.get(url, function(data) {
+
+		// iterate over the returned courses
+		var courses = data.courses;
+		$(courses).each(function(index, course) {
+
+			// append each course to the content of the element
+			$('#course-results').append('&lt;p&gt;' + course.subject + ' ' + course.catalog_number + '&lt;/p&gt;');
+
+		});
+		
+	});
+
+});
+			</code>
+			</pre>
+		</dd>
+		<dt class="accordion__header"> PHP <i class="fa fa-chevron-down fa-pull-right type--red" aria-hidden="true"></i></dt>
+		<dd class="accordion__content">
+    	<pre>
+    	<code class="prettyprint lang-php">
+// query all the information for Computer Science
+$url = '{{ url('api/departments/189') }}';
+
+// call url, you can also use CURL or guzzle -> https://github.com/guzzle/guzzle
+$data = file_get_contents($url);
+
+// decode into an array
+$data = json_decode($data, true);
+
+// setup a blank array
+$course_list = [];
+
+// loop through results and add each courses's subject
+// and catalog number to course_list array (i.e. COMP 100)
+foreach($data['courses'] as $course){
+	$course_list[] = $course['subject'].' '.$course['catalog_number'];
+}
+
+print_r($course_list);
+			</code>
+			</pre>
+		</dd>
+		<dt class="accordion__header"> Python <i class="fa fa-chevron-down fa-pull-right type--red" aria-hidden="true"></i></dt>
+		<dd class="accordion__content">
+	    	<pre>
+	    	<code class="prettyprint lang-py">
+#python
+import urllib2
+import json
+
+#query all the information for Computer Science
+url = u'{{ url('api/departments/189') }}'
+
+#try to read the data	
+try:
+   u = urllib2.urlopen(url)
+   data = u.read()
+except Exception as e:
+	data = {}
+
+#decode into an array
+data = json.loads(data)
+
+#setup a blank array
+course_list = []
+
+#loop through results and add each course's subject
+#and catalog number to course_list array (i.e COMP 100)
+for course in data['courses']:
+	course_list.append(course['subject'] + ' ' + course['catalog_number'])
+
+print course_list
+			</code>
+			</pre>
+		</dd>
+		<dt class="accordion__header"> Ruby <i class="fa fa-chevron-down fa-pull-right type--red" aria-hidden="true"></i></dt>
+		<dd class="accordion__content">
+	    	<pre>
+	    	<code class="prettyprint lang-rb">
+require 'net/http'
+require 'json'
+
+#query all the information for Computer Science
+source = '{!! url('api/departments/189') !!}'
+
+#call data
+response = Net::HTTP.get_response(URI.parse(source))
+
+#get body of the response
+data = response.body
+
+#put the parsed data
+puts JSON.parse(data)
+			</code>
+	    	</pre>
+		</dd>
+	</dl>
 @stop
