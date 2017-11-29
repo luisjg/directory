40dc0d05816d9fd6d4f56c6631134776bba4bb2c
luisjg <luisjguzman87@gmail.com>
2017-11-29 15:48:53 -0800

    Add the NemoEntity model.

---
+++ app/Models/NemoEntity.php
@@ -0,0 +1,26 @@
+<?php namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NemoEntity extends Model {
+
+	/**
+	 * The name of the table in the database
+	 * @var string
+	 */
+	protected $table = 'nemo.entities';
+
+
+    /**
+     * Set the primary key
+     * @var string
+     */
+	protected $primaryKey = 'entities_id';
+
+    /**
+     * Turn the auto-incrementing
+     * @var bool
+     */
+	public $incrementing = false;
+
+}
