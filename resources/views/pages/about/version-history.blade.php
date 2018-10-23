2411503939a6cca6c1709c43b8145762980a055b
luisjg <luis.guzman@metalab.csun.edu>
2018-10-23 10:38:03 -0700

    Update the landing pages and modify the API key check.

--- resources/views/pages/about/version-history.blade.php
+++ resources/views/pages/about/version-history.blade.php
@@ -1,22 +1,34 @@
 @extends('layouts.master')
+
+@section('title')
+    Version History
+@endsection
+
+@section('description')
+    {{ env('APP_NAME') }} Web Service Version History
+@endsection
+
 @section('content')
-	<h2 class="type--header type--thin">Version History</h2>
-	<h2>Directory 1.0.0 <small>Release Date: 02/06/18</small></h2>
-	<p>
-		<strong>New Features:</strong>
-		<ul>
-		<li>Ability to retrieve faculty member listing by department</li>
-		<li>Ability to filter campus faculty member listing by last name</li>
-		<li>Ability to filter faculty member listings by status: all, tenure-track, emeriti, or lecturers</li>
-		<li>Ability to retrieve faculty member degree information</li>
-	</ul>
-	</p>
-	<hr>
-	<h2>Directory Beta <small>Release Date: 12/12/17</small></h2>
-	<p>
-		<strong>New Features:</strong>
-	<ol>
-		<li>Ability to POST and create new users to the Databases.</li>
-	</ol>
-	</p>
+    <h2>Version History</h2>
+    <h3 class="h5 padding">{{ env('APP_NAME') }} 1.0.1 <small>Release Date: 10/23/18 </small></h3>
+    <strong>Improvements:</strong>
+    <ol>
+        <li>Update the landing pages to include the latest version of <a href="//csun-metalab.github.io/metaphorV2/">Metaphor</a>.</li>
+    </ol>
+    <hr class="margin">
+    <h3 class="h5 padding">{{ env('APP_NAME') }} 1.0.0 <small>Release Date: 02/06/18 </small></h3>
+    <strong>New Features:</strong>
+    <ul>
+        <li>Ability to retrieve faculty member listing by department</li>
+        <li>Ability to filter campus faculty member listing by last name</li>
+        <li>Ability to filter faculty member listings by status: all, tenure-track, emeriti, or lecturers</li>
+        <li>Ability to retrieve faculty member degree information</li>
+    </ul>
+    <hr class="margin">
+    <h3 class="h5 padding">{{ env('APP_NAME') }} Beta <small>Release Date: 12/12/17</small></h3>
+    <strong>New Features:</strong>
+    <ol>
+        <li>Ability to POST and create new users to the Databases.</li>
+    </ol>
+    <hr class="margin">
 @endsection
