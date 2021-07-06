308a0361a3dcbb1c4413cc6776d5b622bf8f4ef8
luisjg <luis.guzman@csun.edu>
2021-07-06 11:37:06 -0700

    Include the CSUN footer.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -48,6 +48,7 @@
 			</div>
 		</div>
 	</div>
+	@include('layouts.partials.csun-footer')
 	<script src="{!! url('js/manifest.js') !!}"></script>
 	<script src="{!! url('js/vendor.js') !!}"></script>
 	<script src="{!! url('js/app.js') !!}"></script>
